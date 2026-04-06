201b12e4864a34b24614932dd3ed7e9bc9148b4b
wahidikqbal <wahidikqbal@gmail.com>
2026-04-07 05:27:25 +0700

    menambahkan seeder

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,43 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Myappv1.Repo
+alias Myappv1.Inventory
+
+# Hapus data existing untuk menghindari duplikat saat menjalankan ulang seeder
+Repo.delete_all(from c in Inventory.Category, where: c.id > 0)
+Repo.delete_all(from t in Inventory.Tag, where: t.id > 0)
+
+# ============ SEED CATEGORIES ============
+category_names = [
+  "Handheld",
+  "Base Station",
+  "Mobile",
+  "Repeater",
+  "Scanning",
+]
+
+for name <- category_names do
+  Inventory.create_category(%{name: name})
+end
+
+# ============ SEED TAGS ============
+tag_names = [
+  "VHF",
+  "UHF",
+  "HF",
+  "DMR",
+  "Analog",
+  "Digital",
+  "PMR446",
+  "CB Radio",
+  "Amateur",
+  "Commercial",
+]
+
+for name <- tag_names do
+  Inventory.create_tag(%{name: name})
+end
+
+IO.puts("Seeded #{length(category_names)} categories and #{length(tag_names)} tags!")
