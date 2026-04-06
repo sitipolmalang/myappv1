24eacaa9efddc2b083c8ddf4f1e7a082ec847f53
wahidikqbal <wahidikqbal@gmail.com>
2026-04-07 05:19:17 +0700

    menambahkan komntar pada inventory.ex

--- lib/myappv1/inventory.ex
+++ lib/myappv1/inventory.ex
@@ -1,6 +1,8 @@
 defmodule Myappv1.Inventory do
   @moduledoc """
-  The Inventory context.
+  Modul Inventory berfungsi sebagai konteks/data layer untuk mengelola data radio, kategori, dan tag.
+  Module ini menyediakan fungsi-fungsi untuk operasi CRUD (Create, Read, Update, Delete)
+  terhadap entitas-entitas tersebut menggunakan Ecto dan Repo.
   """
 
   import Ecto.Query, warn: false
@@ -11,34 +13,20 @@ defmodule Myappv1.Inventory do
   alias Myappv1.Inventory.Category
   alias Myappv1.Inventory.Tag
 
+  # ============ FUNGSI CRUD UNTUK RADIO ============
+
   @doc """
-  Returns the list of radios.
-
-  ## Examples
-
-      iex> list_radios()
-      [%Radio{}, ...]
-
+  Mengambil semua data radio dari database.
+  Seluruh data radio akan dimuat beserta relasi category dan tags-nya.
   """
   def list_radios do
     Repo.all(Radio)
     |> Repo.preload([:category, :tags])
-
   end
 
   @doc """
-  Gets a single radio.
-
-  Raises `Ecto.NoResultsError` if the Radio does not exist.
-
-  ## Examples
-
-      iex> get_radio!(123)
-      %Radio{}
-
-      iex> get_radio!(456)
-      ** (Ecto.NoResultsError)
-
+  Mengambil satu data radio berdasarkan ID.
+  Jika radio tidak ditemukan, akan melempar exception Ecto.NoResultsError.
   """
   def get_radio!(id) do
     Repo.get!(Radio, id)
@@ -46,16 +34,8 @@ defmodule Myappv1.Inventory do
   end
 
   @doc """
-  Creates a radio.
-
-  ## Examples
-
-      iex> create_radio(%{field: value})
-      {:ok, %Radio{}}
-
-      iex> create_radio(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Membuat data radio baru dengan atribut yang diberikan.
+  Contoh attrs: %{name: "Radio Kita", category_id: 1, tag_ids: [1, 2, 3]}
   """
   def create_radio(attrs) do
     %Radio{}
@@ -65,16 +45,8 @@ defmodule Myappv1.Inventory do
   end
 
   @doc """
-  Updates a radio.
-
-  ## Examples
-
-      iex> update_radio(radio, %{field: new_value})
-      {:ok, %Radio{}}
-
-      iex> update_radio(radio, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Memperbarui data radio yang sudah ada.
+  Perlu melakukan preload tags terlebih dahulu agar relasi dapat diperbarui dengan benar.
   """
   def update_radio(%Radio{} = radio, attrs) do
     radio
@@ -89,41 +61,32 @@ defmodule Myappv1.Inventory do
   end
 
   @doc """
-  Deletes a radio.
-
-  ## Examples
-
-      iex> delete_radio(radio)
-      {:ok, %Radio{}}
-
-      iex> delete_radio(radio)
-      {:error, %Ecto.Changeset{}}
-
+  Menghapus data radio dari database.
   """
   def delete_radio(%Radio{} = radio) do
     Repo.delete(radio)
   end
 
   @doc """
-  Returns an `%Ecto.Changeset{}` for tracking radio changes.
-
-  ## Examples
-
-      iex> change_radio(radio)
-      %Ecto.Changeset{data: %Radio{}}
-
+  Membuat changeset untuk radio (digunakan dalam form untuk validasi dan tracking perubahan).
+  attrs adalah map optional yang berisi data yang ingin diubah.
   """
   def change_radio(%Radio{} = radio, attrs \\ %{}) do
     radio = with_tag_ids(radio)
     Radio.changeset(radio, attrs)
   end
 
+  # Mengubah struktur tags menjadi list ID untuk form handling (atau "untuk kenyamanan form handling")
   defp with_tag_ids(%Radio{tags: tags} = radio) when is_list(tags) do
     %{radio | tag_ids: Enum.map(tags, & &1.id)}
   end
 
   defp with_tag_ids(radio), do: radio
 
+  # ============ HELPER: Mengambil tag berdasarkan tag_ids ============
+
+  # Fungsi ini mengambil tag IDs dari attrs (support string key dan atom key),
+  # lalu mencari tag-tag tersebut di database.
   defp tags_for_attrs(attrs) do
     attrs
     |> Map.get("tag_ids", Map.get(attrs, :tag_ids, []))
@@ -136,6 +99,7 @@ defmodule Myappv1.Inventory do
     end
   end
 
+  # Parse tag_id dari berbagai tipe: integer, binary/string, atau lainnya
   defp parse_tag_id(id) when is_integer(id), do: id
   defp parse_tag_id(id) when is_binary(id) do
     case Integer.parse(id) do
@@ -143,49 +107,26 @@ defmodule Myappv1.Inventory do
       _ -> nil
     end
   end
-
   defp parse_tag_id(_), do: nil
 
+  # ============ FUNGSI CRUD UNTUK CATEGORY ============
+
   @doc """
-  Returns the list of categories.
-
-  ## Examples
-
-      iex> list_categories()
-      [%Category{}, ...]
-
+  Mengambil semua data kategori dari database.
   """
   def list_categories do
     Repo.all(Category)
   end
 
   @doc """
-  Gets a single category.
-
-  Raises `Ecto.NoResultsError` if the Category does not exist.
-
-  ## Examples
-
-      iex> get_category!(123)
-      %Category{}
-
-      iex> get_category!(456)
-      ** (Ecto.NoResultsError)
-
+  Mengambil satu data kategori berdasarkan ID.
+  Jika kategori tidak ditemukan, akan melempar exception Ecto.NoResultsError.
   """
   def get_category!(id), do: Repo.get!(Category, id)
 
   @doc """
-  Creates a category.
-
-  ## Examples
-
-      iex> create_category(%{field: value})
-      {:ok, %Category{}}
-
-      iex> create_category(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Membuat data kategori baru.
+  Contoh attrs: %{name: "Handheld", description: "Radio genggam"}
   """
   def create_category(attrs) do
     %Category{}
@@ -194,16 +135,7 @@ defmodule Myappv1.Inventory do
   end
 
   @doc """
-  Updates a category.
-
-  ## Examples
-
-      iex> update_category(category, %{field: new_value})
-      {:ok, %Category{}}
-
-      iex> update_category(category, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Memperbarui data kategori yang sudah ada.
   """
   def update_category(%Category{} = category, attrs) do
     category
@@ -212,29 +144,14 @@ defmodule Myappv1.Inventory do
   end
 
   @doc """
-  Deletes a category.
-
-  ## Examples
-
-      iex> delete_category(category)
-      {:ok, %Category{}}
-
-      iex> delete_category(category)
-      {:error, %Ecto.Changeset{}}
-
+  Menghapus data kategori dari database.
   """
   def delete_category(%Category{} = category) do
     Repo.delete(category)
   end
 
   @doc """
-  Returns an `%Ecto.Changeset{}` for tracking category changes.
-
-  ## Examples
-
-      iex> change_category(category)
-      %Ecto.Changeset{data: %Category{}}
-
+  Membuat changeset untuk kategori (digunakan dalam form).
   """
   def change_category(%Category{} = category, attrs \\ %{}) do
     Category.changeset(category, attrs)
@@ -242,46 +159,24 @@ defmodule Myappv1.Inventory do
 
   alias Myappv1.Inventory.Tag
 
+  # ============ FUNGSI CRUD UNTUK TAG ============
+
   @doc """
-  Returns the list of tags.
-
-  ## Examples
-
-      iex> list_tags()
-      [%Tag{}, ...]
-
+  Mengambil semua data tag dari database.
   """
   def list_tags do
     Repo.all(Tag)
   end
 
   @doc """
-  Gets a single tag.
-
-  Raises `Ecto.NoResultsError` if the Tag does not exist.
-
-  ## Examples
-
-      iex> get_tag!(123)
-      %Tag{}
-
-      iex> get_tag!(456)
-      ** (Ecto.NoResultsError)
-
+  Mengambil satu data tag berdasarkan ID.
+  Jika tag tidak ditemukan, akan melempar exception Ecto.NoResultsError.
   """
   def get_tag!(id), do: Repo.get!(Tag, id)
 
   @doc """
-  Creates a tag.
-
-  ## Examples
-
-      iex> create_tag(%{field: value})
-      {:ok, %Tag{}}
-
-      iex> create_tag(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Membuat data tag baru.
+  Contoh attrs: %{name: "VHF", color: "#FF0000"}
   """
   def create_tag(attrs) do
     %Tag{}
@@ -290,16 +185,7 @@ defmodule Myappv1.Inventory do
   end
 
   @doc """
-  Updates a tag.
-
-  ## Examples
-
-      iex> update_tag(tag, %{field: new_value})
-      {:ok, %Tag{}}
-
-      iex> update_tag(tag, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Memperbarui data tag yang sudah ada.
   """
   def update_tag(%Tag{} = tag, attrs) do
     tag
@@ -308,29 +194,14 @@ defmodule Myappv1.Inventory do
   end
 
   @doc """
-  Deletes a tag.
-
-  ## Examples
-
-      iex> delete_tag(tag)
-      {:ok, %Tag{}}
-
-      iex> delete_tag(tag)
-      {:error, %Ecto.Changeset{}}
-
+  Menghapus data tag dari database.
   """
   def delete_tag(%Tag{} = tag) do
     Repo.delete(tag)
   end
 
   @doc """
-  Returns an `%Ecto.Changeset{}` for tracking tag changes.
-
-  ## Examples
-
-      iex> change_tag(tag)
-      %Ecto.Changeset{data: %Tag{}}
-
+  Membuat changeset untuk tag (digunakan dalam form).
   """
   def change_tag(%Tag{} = tag, attrs \\ %{}) do
     Tag.changeset(tag, attrs)
