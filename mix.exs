55da8d3234395d07183a9081e208852ad351c10c
wahidikqbal <wahidikqbal@gmail.com>
2026-04-08 04:13:22 +0700

    menambahkan waffle dan waffle ecto

--- mix.exs
+++ mix.exs
@@ -65,7 +65,9 @@ defmodule Myappv1.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:waffle, "~> 1.1"},
+      {:waffle_ecto, "~> 0.0.11"},
     ]
   end
 
